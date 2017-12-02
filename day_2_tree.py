3286f16d0d2d70d6224819c8a4fe6d52f0b6281b
Paul Brennan <brennanpincardiff@users.noreply.github.com>
2017-12-02 11:00:36 +0000

    Improving tree script with comments

--- day_2_tree.py
+++ day_2_tree.py
@@ -1,17 +1,18 @@
 import turtle
-s=turtle.Screen()
 t=turtle.Turtle()
 t.color("darkgreen")
-#one way to draw a tree
+
+#  green triangles first
 t.begin_fill()
 points=[[0,150], [-30,100], [30,100], [0,150],
         [0,100], [-45,50], [45,50], [0,100],
         [0, 50], [-50, 0], [50,0], [0,50]]
-
+# this is a for loop
 for each in points:
   t.goto(each)
 t.end_fill()
 
+# then our box at the bottom
 t.penup()
 t.color("brown")
 t.begin_fill()
@@ -20,5 +21,6 @@ for each in points:
   t.goto(each)
 t.end_fill()
 
+# could you put yesterday's star on the top?
+turtle.exitonclick()
 
-input()
